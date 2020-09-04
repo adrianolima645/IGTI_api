f31787cee97ac605164f9db56b960665398fe98c
adriano.lima <adrianolima645@gmail.com>
2020-09-03 22:56:23 -0300

    alteracoes gerais

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:8081',
+    origin: 'https://grades-api-645.herokuapp.com',
   })
 );
 
